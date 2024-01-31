35ea94bb14ba2808a3e88676c2782de9498c936f
Raúl Morales <raulmdaw@gmail.com>
2024-01-31 21:09:24 +0100

    294. Obtener las coordenadas centrales del mapa

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,14 +1,15 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap?: ElementRef;
   public zoom: number = 10;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-5.980412, 37.356938);
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento HTML divMap no fue encontrado';
@@ -16,23 +17,34 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-5.980412, 37.356938], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListeners();
   }
 
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
   mapListeners() {
     if (!this.map) throw 'El mapa no se ha inicializado';
 
+    // cambiar valor del zoom en la caja de control
     this.map.on('zoom', (event) => {
       this.zoom = this.map!.getZoom();
     });
 
+    // controlar el valor máximo del zoom, cuando el zoom se detiene
     this.map.on('zoomend', (event) => {
       if (this.map!.getZoom() < 18 ) return;
       this.map!.zoomTo(18);
+    });
+
+    // controlar el movimiento del mapa
+    this.map.on('move', (event) => {
+      this.currentLngLat = this.map!.getCenter();
     });
   }
 
