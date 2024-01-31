624fc6aeee2e2f22f16249b4309e9586813809f6
Raúl Morales <raulmdaw@gmail.com>
2024-01-31 21:32:04 +0100

    295. Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-5.980412, 37.356938);
 
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML divMap no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Raul';
+
+    // const marker = new Marker({
+    //   color: 'teal',
+    //   element: markerHtml
+    // })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map);
+  }
 }
