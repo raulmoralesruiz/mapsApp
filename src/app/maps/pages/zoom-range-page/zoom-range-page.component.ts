a6a384eb4212294540f2cfb85d18e11ce3ff7d4d
Raúl Morales <raulmdaw@gmail.com>
2024-01-31 20:58:30 +0100

    293. Controlar el nivel del Zoom

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -7,15 +7,45 @@ import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 })
 export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map') divMap?: ElementRef;
+  public zoom: number = 10;
+  public map?: Map;
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento HTML divMap no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-5.980412, 37.356938], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if (!this.map) throw 'El mapa no se ha inicializado';
+
+    this.map.on('zoom', (event) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (event) => {
+      if (this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 }
