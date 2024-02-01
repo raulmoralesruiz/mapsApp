8e22fb8336a49a49226c5b94f154969edd55ca9b
Raúl Morales <raulmdaw@gmail.com>
2024-02-01 22:17:34 +0100

    302. Componente MiniMapa

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
 
+  ngAfterViewInit(): void {
+    if (!this.divMap?.nativeElement) throw 'Map Div no encontrado';
+    if (!this.lngLat) throw 'LngLat no puede ser nulo';
+
+    // crear mapa
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      interactive: false,
+    });
+
+    // crear marker inicial
+    new Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map);
+  }
 }
